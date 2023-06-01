135010a4f7202d09fc66da49f7a305a5bdb1ef49
mao <1296193245@qq.com>
2023-06-01 22:22:37 +0800

    java_report_POI_import_excel

---
+++ src/main/java/mao/Test2.java
@@ -0,0 +1,95 @@
+package mao;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+
+/**
+ * Project name(项目名称)：java报表_POI导入excel
+ * Package(包名): mao
+ * Class(类名): Test2
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2023/6/1
+ * Time(创建时间)： 21:54
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+public class Test2
+{
+    public static void main(String[] args)
+    {
+        try (final FileInputStream fileInputStream = new FileInputStream("./class.xlsx"))
+        {
+            //加载工作簿
+            Workbook workbook = new XSSFWorkbook(fileInputStream);
+            //读取第一个工作表
+            Sheet sheet = workbook.getSheetAt(0);
+            //得到最后一行
+            int lastRowNum = sheet.getLastRowNum();
+            //得到第0行
+            Row row = sheet.getRow(0);
+            //校验表头
+            String stringCellValue = row.getCell(0).getStringCellValue();
+            if (!"班级编号".equals(stringCellValue))
+            {
+                throw new UnsupportedOperationException("表格格式不支持");
+            }
+
+            stringCellValue = row.getCell(1).getStringCellValue();
+            if (!"班级名称".equals(stringCellValue))
+            {
+                throw new UnsupportedOperationException("表格格式不支持");
+            }
+
+            stringCellValue = row.getCell(2).getStringCellValue();
+            if (!"班级人数".equals(stringCellValue))
+            {
+                throw new UnsupportedOperationException("表格格式不支持");
+            }
+
+            stringCellValue = row.getCell(3).getStringCellValue();
+            if (!"所属年级".equals(stringCellValue))
+            {
+                throw new UnsupportedOperationException("表格格式不支持");
+            }
+
+            stringCellValue = row.getCell(4).getStringCellValue();
+            if (!"所属学院".equals(stringCellValue))
+            {
+                throw new UnsupportedOperationException("表格格式不支持");
+            }
+
+            System.out.println("班级编号\t\t班级名称\t\t班级人数\t\t所属年级\t\t所属学院");
+
+            //遍历读取数据
+            for (int i = 1; i <= lastRowNum; i++)
+            {
+                //读取第i行
+                row = sheet.getRow(i);
+                //读取班级编号
+                int classNo = ((Double) row.getCell(0).getNumericCellValue()).intValue();
+                //读取班级名称
+                String className = row.getCell(1).getStringCellValue();
+                //读取班级人数
+                int classCount = ((Double) row.getCell(2).getNumericCellValue()).intValue();
+                //读取所属年级
+                int grade = ((Double) row.getCell(3).getNumericCellValue()).intValue();;
+                //读取所属学院
+                String academy = row.getCell(4).getStringCellValue();
+                System.out.println(classNo + "\t\t" + className + "\t\t" + classCount + "\t\t" + grade + "\t\t" + academy);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
